7a03036c56b04eec21e02aa51c0415d101c9506d
robocoonie <lee@recycledsushi.com>
2015-04-20 21:07:01 -0700

    Progress on the JS thingies.

---
+++ javascript/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -0,0 +1,8 @@
+//$("#main").append(["Lee Almodovar"]);
+//[string].replace([old], [new]);
+
+var awesomeThoughts = "I am Lee and I am AWESOME!"
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN!"); 
+$("#main").append(funThoughts);
