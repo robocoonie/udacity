784f8d95adb9e5a77a0c5ffb8273eb1678c619dd
robocoonie <lee@recycledsushi.com>
2015-06-08 23:23:53 -0700

    Python Lesson 1

---
+++ python/take-a-break.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+i = 1
+a = 0
+
+print("This program started on: "+time.ctime())
+while(i < a):
+    time.sleep(10)
+    webbrowser.open("http://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    i = i + 1
