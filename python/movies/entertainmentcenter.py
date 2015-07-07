9d6549fcc5d4936591fcd07eab202adf79f740cc
robocoonie <lee@recycledsushi.com>
2015-07-06 23:34:12 -0700

    Movies website start.

---
+++ python/movies/entertainmentcenter.py
@@ -0,0 +1,41 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "This movie was stupid, don't watch it.",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+space2001 = media.Movie("2001: A Space Odyssey",
+                        "Weird stuff happens because Kubrick and Clarke",
+                        "https://upload.wikimedia.org/wikipedia/en/e/ef/2001_A_Space_Odyssey_Style_B.jpg",
+                        "https://www.youtube.com/watch?v=Ok32VyEQYYc")
+#print(space2001.storyline)
+#space2001.show_trailer()
+
+school_of_rock = media.Movie("School of Rock", "Old rocker transfers his knowledge to some kids",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "A rat shows a chef stuff about cooking",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "I don't know what this is",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+
+hunger_games = media.Movie("Hunger Games", "My friend Christine really likes this movie and the books",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+movies = [toy_story, avatar, space2001, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
