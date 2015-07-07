68de00272dc473719c450d0834b30b3465ddcc7c
Lee Almodovar <lee@recycledsushi.com>
2015-07-07 00:07:45 -0700

    Removing certain comments.

--- python/movies/entertainmentcenter.py
+++ python/movies/entertainmentcenter.py
@@ -38,6 +38,6 @@ hunger_games = media.Movie("Hunger Games", "My friend Christine really likes thi
                            "https://www.youtube.com/watch?v=PbA63a7H0bo")
 
 movies = [toy_story, avatar, space2001, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
-#fresh_tomatoes.open_movies_page(movies)
+fresh_tomatoes.open_movies_page(movies)
 #print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
+#print(media.Movie.__doc__)
