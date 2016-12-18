41dd63243b8d3670746f23c1468a4bb5f1ffdfb9
robocoonie <lee@recycledsushi.com>
2016-12-17 17:36:21 -0800

    Resuming lessons

--- javascript/frontend-nanodegree-resume/js/resumeBuilder.js
+++ javascript/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,5 +1,5 @@
 var name = "Lee Almodovar";
-var role = "QA Automation Engineer";
+var role = "Senior QA Test Engineer";
 
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
@@ -10,57 +10,47 @@ $("#header").prepend(formattedName);
 
 var bio = {
 	"name":"Lee Almodovar",
-	"role":"QA Automation Engineer",
+	"role":"Senior QA Test Engineer",
 	"contacts": {
-		"email":"lee@recycledsushi.com",
+		"email":"me@leealmodovar.com",
 		"mobile":"562-360-0451",	
 		"twitter":"@Lee_Almodovar",
 		"location":"Los Angeles"
 	},
-	"biopic":"https://dl.dropboxusercontent.com/u/29329110/BlogAssets/WorkLee.jpg",
-	"welcome message":"Hi, I'm Lee Almodovar. I help software developers find the little quirks that make their applications stop functioning.",
-	"skills":["JIRA Administration", "Selenium Automation", "WebDriver", "Grunt", "Mocha", "Javascript Automation"]
-};
+	"bioPic":"http://i.imgur.com/r1wfADf.png",
+	"welcomeMessage":"Hi, I'm Lee Almodovar. I help software developers find the little quirks that make their applications stop functioning.",
+	"skills":["JIRA Administration", "Selenium WebDriver", "Mobile Testing", "Manual Testing", "Mocha", "Javascript"]
+}
+
+$("#main").append(bio.name);
 
 var work = {};
-work.position = "QA Lead (Automation)";
-work.employer = "iCrossing";
-work.years = 0.08;
+work.position = "Sr. QA Test Engineer";
+work.employer = "Hallmark Spiritclips";
+work.years = 0.5;
 
 var education = {
 	"schools": [
 		{
 			"name":"Long Beach City College",
 			"city":"Long Beach, CA",
-			"degree":"Transfer",
-			"major":[
-				"Computer Science",
-				"Psychology"
-			],
+			"degree":"Transfer, Incomplete",
+			"major":"Computer Science",
 			"years":"2000-2006"
-		},
-		{
-			"name":"Art Institute of Los Angeles",
-			"city":"Santa Monica, CA",
-			"degree":"Incomplete",
-			"major":[
-				"Web Design"
-			],
-			"years":"2010-2012"
-		},
+		}
 	],
 
 	"onlineCourses": [
 		{
 			"title":"JavaScript Basics",
 			"school":"Udacity",
-			"dates":"2015",
+			"dates":"2016",
 			"url":"http://www.udacitycom/course/ud804"
 		}
 	]
 }
 
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+$("#main").append(work.position);
+$("#main").append(education.schools.name);
 
 
